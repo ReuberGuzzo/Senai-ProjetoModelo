3445eb8fd17af7dad1958b7d81096de666983516
Reuber Guzzo <reuber.rg@gmail.com>
2022-10-17 21:53:59 -0300

    Criando acesso ao banco de dados do mysql

---
+++ Model/AppModelo.Model.Infra/Repositories/NacionalidadeRepository.cs
@@ -0,0 +1,28 @@
+﻿using AppModelo.Model.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppModelo.Model.Infra.Repositories
+{
+    public class NacionalidadeRepository
+    {
+        //CRUD - Create - read - update - delete 
+        public bool Inserir () 
+        {
+
+        }
+        public bool Atualizar ()
+        {
+
+        }
+        public bool Remover (){}
+        public List<NacionalidadeEntity> ObterTodos()
+        {
+            var sql = "SELECT *From nacionalidades";
+        }
+        public NacionalidadeEntity ObterporId(){}
+    }
+}
---
+++ Model/AppModelo.Model.Infra/_Sql/create_table_nacionalidades.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE nacionalidade (
+	id int NOT NULL AUTO_INCREMENT,
+	descrição VARCHAR(250) NOT NULL,
+	CONSTRAINT pk_nacionalidade PRIMARY KEY (id)
+);
